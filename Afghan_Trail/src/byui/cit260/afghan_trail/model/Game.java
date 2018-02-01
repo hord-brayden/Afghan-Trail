1766f12d1c4074d401712a5769cb51ebad2a9e9f
jonsirrine <sir15004@byui.edu>
2018-02-01 06:27:19 -0800

    added game class

---
+++ Afghan_Trail/src/byui/cit260/afghan_trail/model/Game.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.afghan_trail.model;
+//TODO import player class
+import java.io.Serializable;
+
+/**
+ *
+ * @author jonsi
+ */
+public class Game implements Serializable{
+    private int progress;
+//  Player player;
+
+    public Game() {
+        
+    }  
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public void setProgress(int progress) {
+        this.progress = progress;
+    }
+/*
+    public Player getPlayer() {
+        return player;
+    }
+
+*/
+
+    @Override
+    public String toString() {
+        return "Game{" + "progress=" + progress + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + this.progress;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (this.progress != other.progress) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
