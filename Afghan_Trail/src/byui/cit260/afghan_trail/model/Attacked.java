75841d72a1b445478a2e567f80a043a018ac0eca
jonsirrine <sir15004@byui.edu>
2018-02-01 15:30:42 -0800

    added event classes

---
+++ Afghan_Trail/src/byui/cit260/afghan_trail/model/Attacked.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.afghan_trail.model;
+
+/**
+ *
+ * @author jonsi
+ */
+public class Attacked extends Event{
+
+    public Attacked() {
+        super();
+    }
+    
+    public Attacked(String description){
+        super(description);
+    }
+    
+    //TODO
+    //fightBack()
+    //attemptFlee()
+    //surrender()
+    
+}
---
+++ Afghan_Trail/src/byui/cit260/afghan_trail/model/DiseaseContraction.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.afghan_trail.model;
+
+/**
+ *
+ * @author jonsi
+ */
+public class DiseaseContraction extends Event{
+
+    public DiseaseContraction() {
+    }
+
+    public DiseaseContraction(String description) {
+        super(description);
+    }
+    
+    //TODO
+    //ignore()
+    //useMedicine()
+    //rest()
+}
