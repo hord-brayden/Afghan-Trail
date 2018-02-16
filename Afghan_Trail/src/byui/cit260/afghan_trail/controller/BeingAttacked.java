fe1c675699ed34f1015f3e5479259def45ce7738
Rizky Abraham <rizkyabraham88@gmail.com>
2018-02-16 09:11:25 -0800

    Test BeingAttacked

--- Afghan_Trail/src/byui/cit260/afghan_trail/controller/BeingAttacked.java
+++ Afghan_Trail/src/byui/cit260/afghan_trail/controller/BeingAttacked.java
@@ -72,5 +72,30 @@ public class BeingAttacked {
         else
             player.setStamina(player.getStamina() - 5);
     }
-    
+
+   public static boolean attacked(int stamina, int progression, int bullets) {
+       if (stamina > 5 && bullets > 5) {
+           if (progression % 5 != 0) {
+                double staminaChance = stamina * 0.6;
+                double chance = 20 + staminaChance;
+                int num = 90;
+                boolean isSuccessful = (num > chance);
+                return isSuccessful;
+           }
+           else 
+               return false;
+       } else 
+           return false;
+   }
 }
+   
+
+
+
+   
+
+            
+       
+   
+ 
+  
