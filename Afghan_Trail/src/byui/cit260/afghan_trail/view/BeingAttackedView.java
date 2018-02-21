3210a302f5cbd882af6db82edb92db18ce3810c3
Brayden <Brayden@DESKTOP-Q0A28JJ>
2018-02-21 04:01:11 -0800

    Updated 02/21/18 Added View Class that complements COntrol Class

---
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/BeingAttackedView.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.afghan_trail.view;
+import byui.cit260.afghan_trail.controller.BeingAttacked;
+/**
+ *
+ * @author Brayden
+ */
+public class BeingAttackedView extends BeingAttacked {
+   
+public BeingAttackedView() {
+    }
+
+    
+    //TODO
+    //ignore()
+    //useMedicine()
+    //rest()
+}
