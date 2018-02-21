54d71732b89c9cd2a4f822d9c52796872c2bf02e
Brayden <Brayden@DESKTOP-Q0A28JJ>
2018-02-21 04:01:00 -0800

    Updated 02/21/18 Added View Class that complements COntrol Class

---
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/DiseaseContractionView.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.afghan_trail.view;
+import byui.cit260.afghan_trail.controller.DiseaseContraction;
+
+/**
+ *
+ * @author Brayden
+ */
+public class DiseaseContractionView extends DiseaseContraction {
+
+
+    public DiseaseContractionView(String description) {
+        
+    }
+    
+    //TODO
+    //ignore()
+    //useMedicine()
+    //rest()
+    
+}
