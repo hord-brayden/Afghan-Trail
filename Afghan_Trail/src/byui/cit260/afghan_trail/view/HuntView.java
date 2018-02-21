a601f7b33262dfa60208fa818b294fb41232084b
Brayden <Brayden@DESKTOP-Q0A28JJ>
2018-02-21 04:00:46 -0800

    Updated 02/21/18 Added View Class that complements COntrol Class

---
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/HuntView.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.afghan_trail.view;
+import byui.cit260.afghan_trail.controller.Hunt;
+/**
+ *
+ * @author Brayden
+ */
+public class HuntView extends Hunt {
+   
+public HuntView() {
+    }
+
+    
+    //TODO
+    //ignore()
+    //useMedicine()
+    //rest()
+}
