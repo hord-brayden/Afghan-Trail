7155ff9c08b9a50b8c5386dc1c68e47081589fb6
Brayden <Brayden@DESKTOP-Q0A28JJ>
2018-02-21 04:01:06 -0800

    Updated 02/21/18 Added View Class that complements COntrol Class

---
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/BrokenWagonView.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.afghan_trail.view;
+import byui.cit260.afghan_trail.controller.BrokenWagon;
+/**
+ *
+ * @author Brayden
+ */
+public class BrokenWagonView extends BrokenWagon {
+   
+public BrokenWagonView() {
+    }
+
+    
+    //TODO
+    //ignore()
+    //useMedicine()
+    //rest()
+}
