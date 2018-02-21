5da0a2e4d8da55fa2d0ecda4dc4314dbd4421598
Brayden <Brayden@DESKTOP-Q0A28JJ>
2018-02-21 04:00:54 -0800

    Updated 02/21/18 Added View Class that complements COntrol Class

---
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/EnterTownView.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.afghan_trail.view;
+import byui.cit260.afghan_trail.controller.EnterTown;
+/**
+ *
+ * @author Brayden
+ */
+public class EnterTownView extends EnterTown {
+   
+public EnterTownView() {
+    }
+
+    
+    //TODO
+    //ignore()
+    //useMedicine()
+    //rest()
+}
