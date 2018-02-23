482474179cab829049513762bf7559e91e9226e0
rizky <rizky@DESKTOP-0VU7HL4>
2018-02-22 20:00:51 -0800

    Edit

--- Afghan_Trail/src/byui/cit260/afghan_trail/view/EnterTownView.java
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/EnterTownView.java
@@ -12,9 +12,48 @@ import byui.cit260.afghan_trail.controller.EnterTown;
  *
  * @author Brayden
  */
-public class EnterTownView extends EnterTown {
+public class EnterTownView {
    
 public EnterTownView() {
+    }
+
+    public static char display(int progress) {
+        String townName = "a town";
+        switch (progress){
+            case 0:
+                townName = "Kandahar";
+                break;
+            case 5:
+                townName = "Kabul";
+                break;
+            case 10:
+                townName = "Mazar-i-Sharif";
+                break;
+            case 15: 
+                townName = "Maymana";
+                break;
+            case 20:
+                townName = "Herat";
+                break;
+            default:
+                townName = "a town";
+        }
+        System.out.print("Player has entered " + townName + "\n");
+        
+        String[] options = {
+            "Continue",
+            "Map",
+            "Talk to shopkeeper",
+            "Rest and Save game"
+        }; 
+        BasicMenu enterTownMenu = new BasicMenu(
+                "You have entered a town", options
+                
+        );
+        String optionString = enterTownMenu.getOptionsString();
+        System.out.println(enterTownMenu.getMessage() + '\n');
+        char userInput = BasicMenu.getUserChar(optionString);
+        return userInput;
     }
 
     
