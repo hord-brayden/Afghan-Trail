158bb9323cc00f7a757aea4ef4a327b32dfbdec4
Rizky Abraham <rizkyabraham88@gmail.com>
2018-02-27 21:38:31 -0800

    Edit enter town & shopkeeper

--- Afghan_Trail/src/byui/cit260/afghan_trail/view/ShopKeeperView.java
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/ShopKeeperView.java
@@ -5,36 +5,66 @@
  */
 package byui.cit260.afghan_trail.view;
 
+import byui.cit260.afghan_trail.controller.Game;
 import byui.cit260.afghan_trail.model.Player;
+import byui.cit260.afghan_trail.view.EnterTownView;
+import java.util.Scanner;
 
 /**
  *
  * @author rizky
  */
 public class ShopKeeperView {
-    
+
 public ShopKeeperView() {
     }
+   
 
-    public static char display(String name) {
-        String[] options = {
-            "Buy",
-            "Sell",
-            "Exit store"
-        }; 
-        String msg = "Hello, " + name;
-        BasicMenu shopKeeperMenu = new BasicMenu(
-                msg, options   
-        );
-        String optionString = shopKeeperMenu.getOptionsString();
-        System.out.println(shopKeeperMenu.getMessage() + '\n');
-        char userInput = BasicMenu.getUserChar(optionString);
-        return userInput;
-    }
+    public static char display(String name,int progress, Player player) {
+       System.out.print("W - Buy\n" +
+                        "A - Sell\n" +
+                        "S - Rob him!\n"+
+                        "D - Exit store\n");
+       
+    String invalidOptionMsg = "INVALID COMMAND, TRY AGAIN";
 
+       Scanner inFile;
+        inFile = new Scanner(System.in);
+        char userChar;
+        boolean wasORd = false;
+        do {
+            userChar = inFile.next().charAt(0);
+            userChar = Character.toLowerCase(userChar);
+            if (userChar == 'w' || 
+                userChar == 'a' || 
+                userChar == 's' || 
+                userChar == 'd')
+                wasORd = true;
+            else
+                System.out.println(invalidOptionMsg);
+        } while (!wasORd);
+        switch(userChar){
+            case 'w':
+                System.out.print("I am sorry we are sold out!\n");
+            break;
+            case 'a':
+                System.out.print("I am sorry we only sell stuff here!\n");
+            break;
+            case 's':
+                System.out.print("Get out of here!\n");
+            break;
+            case 'd':
+                //int progress = Game.getProgress();
+               
+                EnterTownView.display(progress, player);
+            break;
+        }
+                
     
     //TODO
     //ignore()
     //useMedicine()
     //rest()
+    return userChar;
+}
 }
