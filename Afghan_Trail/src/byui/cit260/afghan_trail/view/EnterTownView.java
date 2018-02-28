af651d306e47e0e998485046f4f0bec1b2e83a17
Rizky Abraham <rizkyabraham88@gmail.com>
2018-02-27 21:38:26 -0800

    Edit enter town & shopkeeper

--- Afghan_Trail/src/byui/cit260/afghan_trail/view/EnterTownView.java
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/EnterTownView.java
@@ -8,6 +8,12 @@ import byui.cit260.afghan_trail.model.Inventory;
 import byui.cit260.afghan_trail.model.Player;
 import byui.cit260.afghan_trail.model.Item;
 import byui.cit260.afghan_trail.controller.EnterTown;
+import byui.cit260.afghan_trail.controller.Game;
+import byui.cit260.afghan_trail.controller.Map;
+import byui.cit260.afghan_trail.controller.ShopKeeperController;
+import static byui.cit260.afghan_trail.view.StartProgramView.mainLoop;
+import java.util.Scanner;
+import byui.cit260.afghan_trail.view.ShopKeeperView;
 /**
  *
  * @author Brayden
@@ -16,12 +22,13 @@ public class EnterTownView {
    
 public EnterTownView() {
     }
-
-    public static char display(int progress) {
-        
+    
+    public static char display(int progress, Player player) {
+        String characterName = player.getName();
         /*
             Get correct town name
         */
+        
         String townName = "a town";
         switch (progress){
             case 0:
@@ -46,20 +53,74 @@ public EnterTownView() {
             Display menu
         */
         System.out.print("Player has entered " + townName + "\n");
-        //show map
-        String[] options = {
+        /*String[] options = {
             "Continue",
             "Talk to shopkeeper",
             "Rest and Save game",
             "Exit"
-        }; 
-        BasicMenu enterTownMenu = new BasicMenu(
+        };
+        */
+        //show map
+        String map = Map.displayMap(progress);
+        System.out.print(map);
+        System.out.print(   "W - Continue\n" +
+                            "A - Talk to shopkeeper\n" +
+                            "S - Rest and save game\n"+
+                            "D - Exit\n");
+        
+        /*BasicMenu enterTownMenu = new BasicMenu(
                 "You have entered a town", options
         );
-        String optionString = enterTownMenu.getOptionsString();
-        System.out.println(enterTownMenu.getMessage() + '\n');
-        char userInput = BasicMenu.getUserChar(optionString);
-        return userInput;
+         */ 
+        //public static char getUserChar(String optionsString){
+        String invalidOptionMsg = "INVALID COMMAND, TRY AGAIN";
+        //System.out.println(optionsString);
+        Scanner inFile;
+        inFile = new Scanner(System.in);
+        char userChar;
+        boolean wasORd = false;
+        do {
+            userChar = inFile.next().charAt(0);
+            userChar = Character.toLowerCase(userChar);
+            if (userChar == 'w' || 
+                userChar == 'a' || 
+                userChar == 's' || 
+                userChar == 'd')
+                wasORd = true;
+            else
+                System.out.println(invalidOptionMsg);
+        } while (!wasORd);
+        
+    
+           switch (userChar){
+            //Continue
+            case 'w':
+                //TODO see how to handle continue via testing
+                System.out.print("Leaving town...\n");
+                
+            break;
+
+            //Shopkeeper
+            case 'a':
+                
+               ShopKeeperView.display(characterName, progress, player);
+                //ShopKeeperController.shopKeeper(Player, shopKeeperChar);
+            break;
+
+            //Save game
+            case 's':
+                Game.saveGame();
+               //char shopKeeperChar = ShopKeeperView.display(player.getName());
+               //ShopKeeperController.shopKeeper(player, shopKeeperChar);
+            break; 
+            //exit
+            case 'd':
+                StartProgramView.mainLoop();
+            break;
+        //String optionString = enterTownMenu.getOptionsString();
+        //System.out.println(enterTownMenu.getMessage() + '\n');
+        //char userInput = BasicMenu.getUserChar(optionString);
+        
     }
 
     
@@ -67,4 +128,7 @@ public EnterTownView() {
     //ignore()
     //useMedicine()
     //rest()
+    return userChar;
+
+    }
 }
