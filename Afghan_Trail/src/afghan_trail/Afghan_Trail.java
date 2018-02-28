37bc5fee2aa16d7852ec0a4d52b8d88ad35cde4a
rizky <rizky@DESKTOP-0VU7HL4>
2018-02-28 10:21:37 -0800

    Add banner

--- Afghan_Trail/src/afghan_trail/Afghan_Trail.java
+++ Afghan_Trail/src/afghan_trail/Afghan_Trail.java
@@ -20,6 +20,14 @@ public class Afghan_Trail {
     public static void main(String[] args) {
         
         //starts the game
+        System.out.print("                    __      _                   _______        _ _         \n" +
+"             /\\    / _|    | |                 |__   __|      (_) |        \n" +
+"            /  \\  | |_ __ _| |__   __ _ _ __      | |_ __ __ _ _| |        \n" +
+"           / /\\ \\ |  _/ _` | '_ \\ / _` | '_ \\     | | '__/ _` | | |        \n" +
+"  _   _   / ____ \\| || (_| | | | | (_| | | | |    | | | | (_| | | |  _   _ \n" +
+" (_) (_) /_/    \\_\\_| \\__, |_| |_|\\__,_|_| |_|    |_|_|  \\__,_|_|_| (_) (_)\n" +
+"                       __/ |                                               \n" +
+"                      |___/                                                \n");
         System.out.print("Welcome to Afghan Trail\n");
         StartProgramView.mainLoop();
 
