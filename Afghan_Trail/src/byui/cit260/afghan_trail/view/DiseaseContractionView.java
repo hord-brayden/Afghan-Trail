bf51446c2b75bdd7383b4a5c0b0f96e116022518
jonsi <jonsi@LAPTOP-C038SKDD>
2018-03-07 12:11:06 -0800

    implemented DiseaseContraction into random generated events

--- Afghan_Trail/src/byui/cit260/afghan_trail/view/DiseaseContractionView.java
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/DiseaseContractionView.java
@@ -19,7 +19,7 @@ public class DiseaseContractionView extends BasicView {
     
    
     
-    DiseaseContractionView(){
+    public DiseaseContractionView(){
         super();
         
         String[] options = {
@@ -31,15 +31,16 @@ public class DiseaseContractionView extends BasicView {
         setOptions(options);
         setMessage(message);
     }
-    DiseaseContractionView(String[] options, String message){
+    
+    public DiseaseContractionView(String[] options, String message){
         super(options, message);
     }
     
     public void displayHelp(){
-        //display event help for each event option
-        System.out.print("Disease Contracation Help \n");
-        System.out.print("TAKE MEDICINE lets you exhause some of your medicine"
-                + "supply, but has the chance to heal your herp-aids \n" ); 
+//display event help for each event option
+System.out.print("Disease Contracation Help \n");
+System.out.print("TAKE MEDICINE lets you exhause some of your medicine"
+        + "supply, but has the chance to heal your herp-aids \n" ); 
 System.out.print("IGNORE means you let the disease/sickness ravage your body, "
         + "also you will progress to the next stage of the game, while your "
         + "brain rots aay from syphilis \n");
@@ -49,44 +50,27 @@ System.out.print("REST menas you kick back and gain some stamina \n");
     public void doAction(String[] options, char action,
                          Game game, Player player){
         switch (action){
+            
+            //Take Medicine
             case 'w':
-                System.out.print(options[0]);
-                //controller method for 
-                
+  
+                System.out.print("You chose " + options[0] + "\n");
+                DiseaseContraction.takeMedicine(player);
                 break;
+             
+            //Ignore    
             case 'a':
-                System.out.print(options[1]);
+                
+                System.out.print("You chose " + options[1] + "\n");
+                DiseaseContraction.ignore(player);
                 break;
+             
+            //Rest    
             case 's':
-                System.out.print(options[2]);
-                break;
-            case 'd':
-                System.out.print(options[3]);
+                
+                System.out.print("You chose " + options[2] + "\n");
+                DiseaseContraction.rest(player);
                 break;
         }
-    }
-    
-
-    public DiseaseContractionView(Player player, boolean isSuccessful, Item item) {
-        //Print out to user what the current situation is, and whatever
-        //they input, print out what the result, or process is
-        if (isSuccessful == true) {    
-            //Sickness Cured, you recieve a bonus stamina boost for curing your illness
-                    System.out.print("You are feeling better!\n");
-                    System.out.print("What would you like to do next?\n");
-                    System.out.print("You have! /**item.medicine*/ left.\n");
-            //Have alternate options for the player as to what they can do next
-                    
-        } else{
-                    System.out.print("You are feeling very ill. THis will affect you in many ways.\n");
-                    System.out.print("What would you like to do next?\n");
-            //Have alternate options for the player as to what they can do next
-        }
-    
-    //TODO
-    //ignore()
-    //useMedicine()
-    //rest()
-    
     }
 }
