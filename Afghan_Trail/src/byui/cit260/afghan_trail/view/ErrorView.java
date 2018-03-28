daf9a9f43c15bd61c6cd50f7a885b1e4ed121f8e
jonsirrine <sir15004@byui.edu>
2018-03-28 13:11:52 -0700

    Added errorView class

--- Afghan_Trail/src/byui/cit260/afghan_trail/view/ErrorView.java
+++ Afghan_Trail/src/byui/cit260/afghan_trail/view/ErrorView.java
@@ -5,11 +5,22 @@
  */
 package byui.cit260.afghan_trail.view;
 
+import afghan_trail.Afghan_Trail;
+import java.io.PrintWriter;
+
 /**
  *
  * @author jonsi
  */
 public class ErrorView {
-    //private static PrintWriter console = Afghan_Trail.getOutput()
+    private static PrintWriter console = Afghan_Trail.getOutFile();
+    private static PrintWriter log = Afghan_Trail.getLogFile();
     
+    public static void display(String className, String errorMessage){
+        console.println(
+                "\n---ERROR------------------------------------------"
+              + "\n" + errorMessage
+              + "\n--------------------------------------------------");
+        log.printf("%n$n$s", className + " - " + errorMessage);
+    }
 }
